96f0b471d4eb797323d2f9591c1b04099d4d80b3
Ben Walker <ben.walker@envestnet.com>
2018-08-22 16:15:16 -0700

    Got TFS connection working, added untracked tfs.json as its config

--- src/taskmod.ts
+++ src/taskmod.ts
@@ -1,4 +1,12 @@
+import * as vsts from 'vso-node-api';
+var tfs = require('./tfs.json');
 
 export function sayHello() {
-    console.log('Hello World!');
+    // from excluded tfs.json
+    let collectionUrl = tfs.url;
+    let token: string = tfs.token; 
+
+    let authHandler = vsts.getPersonalAccessTokenHandler(token); 
+    let connection = new vsts.WebApi(collectionUrl, authHandler);
+    console.log("Connection success!");
 }
