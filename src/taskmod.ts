aa4cb6c700f0843ac3af986cbe1887bd5df2cc05
Ben Walker <ben.walker@envestnet.com>
2018-08-27 12:26:45 -0700

    Successfully creating build definition via supported client & api versions

--- src/taskmod.ts
+++ src/taskmod.ts
@@ -1,18 +1,25 @@
 import * as bApi from 'vso-node-api/BuildApi';
+import * as cApi from 'vso-node-api/coreApi';
 import * as buildInterfaces from 'vso-node-api/interfaces/BuildInterfaces';
+import * as coreInterfaces from 'vso-node-api/interfaces/CoreInterfaces';
 import * as escapeStringRegexp from 'escape-string-regexp';
 import * as fs from 'fs';
 import * as tl from 'vsts-task-lib/task';
 import * as vsts from 'vso-node-api';
+import * as util from 'util'
+import { IRequestHandler } from 'vso-node-api/interfaces/common/VsoBaseInterfaces';
 
-export function creatBuildDefinition() {
+export async function creatBuildDefinition() {
     let collectionUrl: string = tl.getInput('tfsurl');
     let token: string = tl.getInput('tfstoken'); 
 
     let authHandler = vsts.getPersonalAccessTokenHandler(token);
-    let handlers = new Array(authHandler);
+    let versionHandler = vsts.getVersionHandler('2.0');
+    let handlers: IRequestHandler[] = [ authHandler, versionHandler ];
     let buildApi = new bApi.BuildApi(collectionUrl, handlers);
+    let coreApi = new cApi.CoreApi(collectionUrl, handlers);
 
+    let currentProject: coreInterfaces.TeamProject = await coreApi.getProject(tl.getInput('projectid'));
     let json: string = fs.readFileSync(tl.getInput('filepath')).toString();
     let branches: string[];
 
@@ -23,33 +30,45 @@ export function creatBuildDefinition() {
         branches = [tl.getInput('targetbranches')];
     }
 
-    branches.forEach(branchName => {
+    // TO-DO: Support multiple target branches
+    // branches.forEach(branchName => {
         let branchPath: string;
-        if((branchName.toLowerCase()).indexOf('release') > -1) {
-            branchPath = tl.getInput('releasetfvcpath') + branchName;
+        if((branches[0].toLowerCase()).indexOf('release') > -1) {
+            branchPath = tl.getInput('releasetfvcpath') + branches[0];
         }
-        else if((branchName.toLowerCase()).indexOf('main') > -1) {
-            branchPath = tl.getInput('basetfvcpath') + branchName;
+        else if((branches[0].toLowerCase()).indexOf('main') > -1) {
+            branchPath = tl.getInput('basetfvcpath') + branches[0];
         }
         else {
-            branchPath = tl.getInput('featuretfvcpath') + branchName;
+            branchPath = tl.getInput('featuretfvcpath') + branches[0];
         }
         
-        // replace all occurences of tokens with actual branch name & path
+        // replaces all occurences of tokens with actual branch name & path
         json = json.replace(
                 new RegExp(
                     escapeStringRegexp(
                         tl.getInput('branchnametoken')
-                    ), 'g'), branchName)
+                    ), 'g'), branches[0])
             .replace(
                 new RegExp(
                     escapeStringRegexp(
                         tl.getInput('branchpathtoken')
                     ), 'g'), branchPath);
-        
+        // TO-DO: Make sure build definition is being created in appropriate branch
         let def: buildInterfaces.BuildDefinition = JSON.parse(json);
-        def.name += "TEST"
-        let data = buildApi.createDefinition(def);
-        console.log(data);
-    });
+        def.name += 'TEST';
+        def.project = {
+            abbreviation: currentProject.abbreviation,
+            description: currentProject.description,
+            id: currentProject.id,
+            name: currentProject.name,
+            revision: currentProject.revision,
+            state: currentProject.state,
+            url: currentProject.url
+        };
+
+        // TO-DO: Add try-cactch block for promise
+        let result = await buildApi.createDefinition(def, currentProject.id);
+        console.log('Result of build definition create is: ' + util.inspect(result, false, null));
+   // });
 }
