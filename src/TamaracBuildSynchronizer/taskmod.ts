909e870cd063395a20732b9b3036ec8733c40c49
Ben Walker <ben.walker@envestnet.com>
2018-09-06 15:45:59 -0700

    Added file globbing support, Thumbs.db to gitignore

--- src/TamaracBuildSynchronizer/taskmod.ts
+++ src/TamaracBuildSynchronizer/taskmod.ts
@@ -1,8 +1,9 @@
 import * as escapeStringRegexp from 'escape-string-regexp';
+import * as glob from 'glob';
 import { BuildApi } from 'vso-node-api/BuildApi';
 import { BuildDefinition, BuildDefinitionReference } from 'vso-node-api/interfaces/BuildInterfaces';
 import { CoreApi } from 'vso-node-api/coreApi';
-import { getInput, getVariable, getTaskVariable } from 'vsts-task-lib/task';
+import { getInput, getVariable, getTaskVariable, filePathSupplied } from 'vsts-task-lib/task';
 import { getPersonalAccessTokenHandler, getVersionHandler } from 'vso-node-api';
 import { inspect } from 'util';
 import { IRequestHandler } from 'vso-node-api/interfaces/common/VsoBaseInterfaces';
@@ -47,14 +48,9 @@ function _getBranchDependentPath(branchPath: string) {
 }
 
 function _getFileNames(filePath: string) {
-    let fileNames: string[] = [];
-    if (lstatSync(filePath).isDirectory()) {
-        readdirSync(filePath).forEach(file => {
-            fileNames.push(filePath + file);
-        })
-    }
-    else {
-        fileNames = [filePath];
+    let fileNames: string[] = glob.sync(filePath);
+    if (fileNames == undefined || fileNames.length == 0) {
+        throw new Error('No files added');
     }
     return fileNames;
 }
@@ -65,22 +61,22 @@ export async function syncBuildDefinitions() {
             getPersonalAccessTokenHandler(getVariable('SYSTEM_ACCESSTOKEN')),
             getVersionHandler(getInput('tfsapiversionnumber', true))
         ];
-        const buildApi = 
+        const buildApi =
             new BuildApi(getVariable('SYSTEM_TEAMFOUNDATIONCOLLECTIONURI'), handlers);
-        const coreApi = 
+        const coreApi =
             new CoreApi(getVariable('SYSTEM_TEAMFOUNDATIONCOLLECTIONURI'), handlers);
         const currentProject: TeamProject =
             await coreApi.getProject(getVariable('SYSTEM_TEAMPROJECTID'));
 
-        let fileNames: string[] = _getFileNames(getInput('filepath', true))!;
+        let fileNames: string[] = _getFileNames(getInput('filepath', true));
         let buildDefs: BuildDefinitionReference[] =
             await buildApi.getDefinitions(currentProject.id);
 
         for (const fileName of fileNames) {
             let json: string = readFileSync(fileName).toString();
             let branchPath: string = getVariable('BUILD_SOURCEBRANCH');
-            let buildDefPath: string = 
-                _getBranchDependentPath(getVariable('BUILD_SOURCEBRANCHNAME')) + 
+            let buildDefPath: string =
+                _getBranchDependentPath(getVariable('BUILD_SOURCEBRANCHNAME')) +
                 getVariable('BUILD_SOURCEBRANCHNAME');
 
             json = _untokenizeJson(getVariable('BUILD_SOURCEBRANCHNAME'), branchPath, json);
@@ -97,7 +93,7 @@ export async function syncBuildDefinitions() {
             result = await buildApi.createDefinition(def, currentProject.id);
             console.log('Result of build definition operation is: \n' +
                 inspect(result, false, null));
-            }
+        }
     }
     catch (e) {
         throw e;
